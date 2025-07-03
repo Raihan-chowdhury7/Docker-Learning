06974307c10a1ae26008a6b233ea0b43ff53422a
Raihan-chowdhury7 <raihan2cho@gmail.com>
2025-07-03 20:32:24 +0100

    Add python code

---
+++ Challenge/count.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import redis
+
+app = Flask(__name__)
+r = redos.Redis(host='redis' , port=6379)
+
+@app.route('/')
+def welcome():
+    return "Welcome to thr CoderCo Container Challenge"
+
+@app.route('/count')
+def count():
+    count = r.incr('visits')
+    return f'This page has been visited {count} times.'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5002)
+
